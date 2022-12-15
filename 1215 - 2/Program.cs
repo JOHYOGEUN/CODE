d2b1b0bd11659445426e860656d7730879c90c1a
JOHYOGEUN <johg940929@gmail.com>
2022-12-15 16:32:48 +0900

    2

---
+++ 1215 - 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1215___2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! 2");
+        }
+    }
+}
